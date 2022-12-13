7e21fb49c99c54934b058230f27b6b948a1fca27
Gilson Alves De Souza <gilsonalvesdesouza@hotmail.com>
2022-12-12 23:11:15 -0300

    finalizado exercicio 3

---
+++ classe-03/index.ts
@@ -0,0 +1,10 @@
+const taAberto = (ano: number, mes: number, dia: number, hora: number) => {
+  const utcDate = new Date(Date.UTC(ano, mes, dia, hora));
+  if (utcDate.getHours() >= 8 && utcDate.getHours() <= 18) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(taAberto(2022, 11, 13, 2));
